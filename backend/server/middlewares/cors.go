ae0c38d2e9ebfc384cca83520bfbd1cee77b930d
k0rean-rand0m <15913538+k0rean-rand0m@users.noreply.github.com>
2024-12-18 17:42:02 +0700

    Launch campaign / Dec 18 Handling x-telegram-init-data header

--- backend/server/middlewares/cors.go
+++ backend/server/middlewares/cors.go
@@ -8,7 +8,7 @@ func EnableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, X-Telegram-Init-Data")
 
 		if r.Method == "OPTIONS" {
 			return
